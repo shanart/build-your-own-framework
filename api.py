ed32af6b0351d86963fad59e2a162c7699777519
shanart <shanin.x@gmail.com>
2020-07-21 17:19:38 +0300

    duplicate routes exception

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-# api.py
 from parse import parse
 from webob import Request, Response
 
@@ -9,23 +8,20 @@ class API:
 
     def __call__(self, environ, start_response):
         request = Request(environ)
+
         response = self.handle_request(request)
+
         return response(environ, start_response)
 
     def route(self, path):
+        if path in self.routes:
+            raise AssertionError("Such route already exists.")
+
         def wrapper(handler):
             self.routes[path] = handler
             return handler
-        return wrapper
 
-    def handle_request(self, request):
-        response = Response()
-        handler, kwargs = self.find_handler(request_path=request.path)
-        if handler is not None:
-            handler(request, response, **kwargs)
-        else:
-            self.default_response(response)
-        return response
+        return wrapper
 
     def default_response(self, response):
         response.status_code = 404
@@ -36,4 +32,17 @@ class API:
             parse_result = parse(path, request_path)
             if parse_result is not None:
                 return handler, parse_result.named
+
         return None, None
+
+    def handle_request(self, request):
+        response = Response()
+
+        handler, kwargs = self.find_handler(request_path=request.path)
+
+        if handler is not None:
+            handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
+
+        return response
