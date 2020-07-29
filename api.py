27cb3abcce9e6b48c897c19b608566a733829f5b
shanart <shanin.x@gmail.com>
2020-07-29 17:48:56 +0300

    jinja2 templates

--- api.py
+++ api.py
@@ -1,15 +1,20 @@
+import os
 import inspect
 
 from parse import parse
 from webob import Request, Response
 from requests import Session as RequestSession
 from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
+from jinja2 import Environment, FileSystemLoader
 
 
 class API:
 
-    def __init__(self):
+    def __init__(self, templates_dir="templates"):
         self.routes = {}
+        self.templates_env = Environment(
+            loader=FileSystemLoader(os.path.abspath(templates_dir))
+        )
 
     def __call__(self, environ, start_response):
         request = Request(environ)
@@ -59,3 +64,8 @@ class API:
         session = RequestSession()
         session.mount(prefix=base_url, adapter=RequestsWSGIAdapter(self))
         return session
+
+    def template(self, template_name, context=None):
+        if context is None:
+            context = {}
+        return self.templates_env.get_template(template_name).render(**context)
--- app.py
+++ app.py
@@ -26,3 +26,11 @@ def handler(req, resp):
     resp.text = "sample"
 
 app.add_route("/sample", handler)
+
+
+@app.route("/template")
+def template_handler(req, resp):
+    resp.body = app.template(
+        "index.html",
+        context={"name": "Template", "title": "Template body"}
+    ).encode()
