953ef52a265af6e35bbcd9de529a4a1d754a5035
shanart <shanin.x@gmail.com>
2020-07-29 18:20:28 +0300

    static files

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import pytest
+from api import API
+
+FILE_DIR = "css"
+FILE_NAME = "main.css"
+FILE_CONTENTS = "body {background-color: red}"
 
 
 def test_basic_route_adding(api):
@@ -119,6 +124,24 @@ def test_custom_exception_hanlder(api, client):
     assert response.text == "AttributeErrorHappened"
 
 
-@app.route("/exception")
-def exception_throwing_handler(request, response):
-    raise AssertionError("This handler should not be used.")
+def test_404_is_returned_for_nonexistent_static_file(client):
+    assert client.get(f"http://testserver/main.css)").status_code == 404
+
+
+def _create_static(static_dir):
+    asset = static_dir.mkdir(FILE_DIR).join(FILE_NAME)
+    asset.write(FILE_CONTENTS)
+
+    return asset
+
+
+def test_assets_are_served(tmpdir_factory):
+    static_dir = tmpdir_factory.mktemp("static")
+    _create_static(static_dir)
+    api = API(static_dir=str(static_dir))
+    client = api.test_session()
+
+    response = client.get(f"http://testserver/{FILE_DIR}/{FILE_NAME}")
+
+    assert response.status_code == 200
+    assert response.text == FILE_CONTENTS
